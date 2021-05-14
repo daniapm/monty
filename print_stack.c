0611152caf2733367e4af3d2f5c4c95358ce9343
Dania Puertas <daniajibellis@hotmail.com>
2021-05-14 00:53:18 +0000

    correcting memory errorscorrecting memory errors

--- print_stack.c
+++ print_stack.c
@@ -12,7 +12,6 @@ void pint_t(sttack_t **head, unsigned int line_number)
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
